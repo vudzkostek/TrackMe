e06c09e135ccb2295f28f3711802a4baf6cdaaca
Artur Kosta <ext-artur.kosta@here.com>
2019-02-13 14:08:44 +0100

    Test cases

--- lib/src/test/java/eu/codingtemple/trackme/TrackMeTest.kt
+++ lib/src/test/java/eu/codingtemple/trackme/TrackMeTest.kt
@@ -1,9 +1,33 @@
 package eu.codingtemple.trackme
 
 import org.assertj.core.api.Assertions
+import org.junit.Before
 import org.junit.Test
+import org.mockito.MockitoAnnotations
 
 class TrackMeTest {
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    // Test cases:
+    // Initialize TrackMe with params
+    // Initialize in threads (with listener), init completed listener
+    // Initialize blocking (with listener), init completed listener
+    // Set consent state to true (from false), listener
+    // Set consent state to false (from true), listener
+    // set consent state true -> true and false -> false
+    // log event to all, blocking
+    // log event to all, in threads
+    // log event to one
+    // silent crashing in init/start/finish
+    // silent crashing in logging
+    // overriding all consents to true
+    // overriding all consents to false
+    // finish all sinks
+
 
     @Test
     fun builderCorrect() {
